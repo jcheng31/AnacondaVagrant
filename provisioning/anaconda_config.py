2dccb186a89ac1602ea35189939d6c77ec0bd7d9
Jerome Cheng <jerome@ayulin.net>
2015-04-11 21:57:15 +0800

    Install git, clone the Anaconda Server, and configure it.

---
+++ provisioning/anaconda_config.py
@@ -0,0 +1,5 @@
+# Change these as desired.
+python_interpreter = "python3"
+project = "AnacondaVagrant"
+extra_paths = None
+port = '19360'  # Must match the guest port in the Vagrantfile.
--- provisioning/base.sh
+++ provisioning/base.sh
@@ -1,4 +1,21 @@
-# Main provisioning script. Updates Python 3, installs pip, and git.
-# git is used to clone the Anaconda server.
-apt-get -y upgrade python3
-apt-get -y install python3-pip git
+# Main provisioning. Installs git, which is used to clone the Anaconda server.
+apt-get -y install git
+
+# Anaconda set-up.
+git clone https://github.com/DamnWidget/anaconda /opt/anaconda
+cd /opt/anaconda
+git checkout vagrant_server
+
+rm /opt/anaconda/config.py
+cp /vagrant/provisioning/anaconda_config.py /opt/anaconda/config.py
+
+# OPTIONAL: Install pip, and dependencies from requirements.txt
+# Uncomment as desired.
+
+# - Python 2
+# apt-get -y install python-pip
+# pip install -r /vagrant/requirements.txt
+
+# - Python 3
+# apt-get -y install python3-pip
+# pip3 install -r /vagrant/requirements.txt
